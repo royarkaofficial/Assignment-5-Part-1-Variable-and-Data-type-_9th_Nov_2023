7fbcf1568bf3c3c003ce6a46a7178dd6b1b977fa
Arkadeep Roy <royarkadeep1112@gmail.com>
2023-11-09 16:36:49 +0530

    This is an assignment on Day 4 Part 2

---
+++ Declare_and_Initialize_variable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Declare_and_Initialize_variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialize variables
+            string name = "Arkadeep"; // Replace with your actual name
+            int age = 23; // Replace with your actual age
+            double height = 173.0; // Replace with your actual height in centimeters
+
+            // Display variables using Console.WriteLine() with appropriate messages
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age + " years");
+            Console.WriteLine("Height: " + height + " centimeters");
+        }
+    }
+}
